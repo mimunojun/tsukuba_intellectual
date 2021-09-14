0d3385830c013a33c2060af92e0b6cfaade7b4f5
mimunojun <mimura.tsukuba@gmail.com>
2021-09-15 02:37:27 +0900

    firebase導入仮

--- module.js
+++ module.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +16,10 @@ const firebaseConfig = {
   storageBucket: "tisikijinweb.appspot.com",
   messagingSenderId: "630058005638",
   appId: "1:630058005638:web:3690efe0a389dddd6a4d07",
+  databassURL: "https://tisikijinweb-default-rtdb.firebaseio.com/",
   measurementId: "G-ZC3DWMM344"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
